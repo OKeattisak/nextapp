51327da1c73393036384809b76fccc99835d9517
Keattisak <OKeattisak.A@outlook.com>
2024-05-05 10:08:19 +0700

    Add Viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 export const metadata = {
   title: "My Mantine app",
   description: "I have followed setup instructions carefully",
+  viewport: "width=device-width, initial-scale=1.0, user-scalable=no"
 };
 
 export default function RootLayout({
