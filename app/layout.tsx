8107f2b501bacdf4bc92e1055dc8bcc2c33512c6
Keattisak <OKeattisak.A@outlook.com>
2024-05-05 19:22:39 +0700

    Add Meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <ColorSchemeScript />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body>
         <MantineProvider>{children}</MantineProvider>
